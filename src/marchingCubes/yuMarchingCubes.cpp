33430358f0a448d8f7051a619138f9201670fe65
yusnows <YusnowsLiang@gmail.com>
2018-07-11 22:31:15 +0800

    switch work place

---
+++ src/marchingCubes/yuMarchingCubes.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: yuMarchingCubes.cpp
+	> Author: yusnows
+	> Mail: YusnowsLiang@gmail.com
+	> Created Time: Wed 11 Jul 2018 06:09:17 PM CST
+    > Copyright@yusnows: All rights reserve!
+ ************************************************************************/
+#include "yuMarchingCubes.h"
+
+namespace sdf
+{
+	
+}
+
---
+++ src/marchingCubes/yuMarchingCubes.h
@@ -0,0 +1,31 @@
+/*************************************************************************
+	> File Name: yuMarchingCubes.h
+	> Author: yusnows
+	> Mail: YusnowsLiang@gmail.com
+	> Created Time: Wed 11 Jul 2018 06:09:30 PM CST
+    > Copyright@yusnows: All rights reserve!
+ ************************************************************************/
+
+#ifndef _YUMARCHINGCUBES_H
+#define _YUMARCHINGCUBES_H
+#include "mcTables.h"
+#include "../tsdf/sdf.h"
+
+namespace sdf
+{
+	class yuMCubes_t :public sdf_t
+	{
+	public:
+		yuMCubes_t(){}
+		~yuMCubes_t(){}
+		int setIsoleval() ;                                    
+		
+	protected:
+		double _isoleval;
+		double _threshold;
+		
+	};
+}
+
+
+#endif
